7688aee47bdcfcceafe576614789412d20b329fd
Vivaldy Augusta M.I <39726092+vlausargs@users.noreply.github.com>
2022-04-03 21:59:55 +0700

    Update bundles.js

--- routes/bundles.js
+++ routes/bundles.js
@@ -4,55 +4,37 @@ const verified = require('./verifyToken')
 const Bundle = require('../models/Bundle')
 const Product = require('../models/Product')
 
-router.get('/', verified, async(req, res) => {
+router.get('/', verified, async (req, res) => {
     try {
         let bundles = await Bundle.find()
-        
+
         for (let index = 0; index < bundles.length; index++) {
             var bundle = bundles[index].toObject();
-            
-       
-
-       
-            for(let i = 0; i < bundle.productID.length; i++) {
-                const query = Product.where({SKU: bundle.productID[i]})
-                query.findOne((err, product) => {
-                    if (err) {
-                        console.log(err)
-                        return res.status(400).send({
-                            "message":"Bad Request " + err
-                        })
-                    }
-                    // console.log(product)
-                    if (product) {
-                        bundle.bundlingStock = product.stock/bundle.qty[i]
-                    }
-                    else {
-                        
-                        bundle['bundlingStock'] = 0
-                        // return res.status(400).send({
-                            //     "message": "Product not found :("
-                            // })
-                        }
-                        
-                        bundles[index] = bundle
-                    }
-                    )
-                    console.log(bundle)
+            bundle.bundlingStock = 0 
+            for (let i = 0; i < bundle.productID.length; i++) {
+                const product = await  Product.find( { SKU: bundle.productID[i] } )
+               
+                // console.log(product)
+                if (product) {
+                    if (bundle.bundlingStock !== 0 )bundle.bundlingStock = Math.min(bundle.bundlingStock, (product[0].stock / bundle.qty[i])) 
+                    else bundle.bundlingStock = (product[0].stock / bundle.qty[i])
+                    console.log(bundle.bundlingStock)
                 }
+                bundles[index] = bundle
+            }
             // return bundle;
-      
-        
-    }
+
+
+        }
         res.json(bundles)
-    } catch(e) {
+    } catch (e) {
         res.status(400).send({
             "message": "Bad Request" + e
         })
     }
 })
 
-router.post('/create', verified, async(req, res) => {
+router.post('/create', verified, async (req, res) => {
     const bundle = new Bundle({
         SKU: req.body.SKU,
         qty: req.body.qty,
@@ -64,19 +46,19 @@ router.post('/create', verified, async(req, res) => {
     let lenProduct = bundle.productID.length
     let lenQty = bundle.qty.length
 
-    if (lenProduct != lenQty ){
+    if (lenProduct != lenQty) {
         return res.status(400).send({
             "message": "Request not valid :|"
         })
     }
 
     for (let i = 0; i < lenProduct; i++) {
-        const query = Product.where({SKU: bundle.productID[i]})
+        const query = Product.where({ SKU: bundle.productID[i] })
         query.findOne((err, product) => {
             if (err) {
                 console.log(err)
                 return res.status(400).send({
-                    "message":"Bad Request " + err
+                    "message": "Bad Request " + err
                 })
             }
             console.log(product)
@@ -99,30 +81,30 @@ router.post('/create', verified, async(req, res) => {
 
         })
     }
-    
+
 
     bundle.save().then((data) => {
         return res.json(data)
     }).catch((err) => {
         console.log("error!")
         res.status(400).send({
-            "message":"Bad Request "+err
+            "message": "Bad Request " + err
         });
     })
-    
+
 })
 
-router.post('/delete/:id',verified,async (req,res)=>{
-    const bundle = Bundle.findById(req.params.id).then(obj =>{
+router.post('/delete/:id', verified, async (req, res) => {
+    const bundle = Bundle.findById(req.params.id).then(obj => {
         return res.json(obj)
-    }).catch(err=>{
+    }).catch(err => {
         console.log("error!")
-        return res.status(400).send({"message":"Bad Request "+err});
+        return res.status(400).send({ "message": "Bad Request " + err });
     })
 })
 
-router.post('/update/:id',verified,async (req,res)=>{
-    const bundle = Bundle.findById(req.params.id).then(obj =>{
+router.post('/update/:id', verified, async (req, res) => {
+    const bundle = Bundle.findById(req.params.id).then(obj => {
         obj.SKU = req.body.SKU
         obj.name = req.body.name
         obj.stock = req.body.stock
@@ -130,17 +112,17 @@ router.post('/update/:id',verified,async (req,res)=>{
         obj.weight = req.body.weight
         obj.UOM = req.body.UOM
         obj.save()
-        .then(
-            (data)=>{
-                return res.json(data)
-            }
-        ).catch(err=>{
-            console.log("error!")
-            return res.status(400).send({"message":"Bad Request "+err});
-        })
-    }).catch(err=>{
+            .then(
+                (data) => {
+                    return res.json(data)
+                }
+            ).catch(err => {
+                console.log("error!")
+                return res.status(400).send({ "message": "Bad Request " + err });
+            })
+    }).catch(err => {
         console.log("error!")
-        return res.status(400).send({"message":"Bad Request "+err});
+        return res.status(400).send({ "message": "Bad Request " + err });
     })
 
 })
